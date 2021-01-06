45ea8ff7ec02e4d738d3e68a6416e0001fb773c6
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-01-06 21:48:50 +0100

    create object to store dom elements

---
+++ Level-two/index.js
@@ -0,0 +1,11 @@
+const domElements = {
+    form: document.getElementById('form'),
+    firstName: document.getElementById('first-name'),
+    lastName: document.getElementById('last-name'),
+    email: document.getElementById('email'),
+    company: document.getElementById('company'),
+    phone: document.getElementById('phone'),
+    message: document.querySelector('textarea'),
+    budget: document.querySelectorAll('input[name="budget"]'),
+    info: document.getElementById('info')
+}
